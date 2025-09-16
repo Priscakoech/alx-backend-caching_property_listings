17ff8238e940ccc5cc01cb75dd3ddedf3ad28d28
Priscakoech <priscakoech448@gmail.com>
2025-09-16 21:37:10 +0300

    added the hit ratio

--- properties/utils.py
+++ properties/utils.py
@@ -7,35 +7,44 @@ logger = logging.getLogger(__name__)
 
 def get_all_properties():
     # Try to fetch from cache
-    properties = cache.get("all_properties")
-
-    if not properties:
-        # Not cached → fetch from DB
-        properties = list(Property.objects.all().values("id", "title", "description", "price"))
-        # Store in Redis for 1 hour (3600 seconds)
-        cache.set("all_properties", properties, 3600)
+    try: 
+        properties = cache.get("all_properties")
+        if not properties:
+            # Not cached → fetch from DB
+            properties = list(Property.objects.all().values("id", "title", "description", "price"))
+            # Store in Redis for 1 hour (3600 seconds)
+            cache.set("all_properties", properties, 3600)
+    except Exception as e:
+        logger.error(f"Error fetching properties: {e}")
+        properties = []
 
     return properties
+
 
 def get_redis_cache_metrics():
     """
     Fetch Redis cache hit/miss metrics and calculate hit ratio.
     """
-    conn = get_redis_connection("default")
-    info = conn.info("stats")
+    try:
+        conn = get_redis_connection("default")
+        info = conn.info("stats")
 
-    hits = info.get("keyspace_hits", 0)
-    misses = info.get("keyspace_misses", 0)
-    total = hits + misses
+        hits = info.get("keyspace_hits", 0)
+        misses = info.get("keyspace_misses", 0)
+        total_requests= hits + misses
 
-    hit_ratio = (hits / total) if total > 0 else 0
+        hit_ratio = (hits / total_requests if total_requests > 0 else 0)
 
-    metrics = {
+        metrics = {
         "hits": hits,
         "misses": misses,
         "hit_ratio": round(hit_ratio, 2),
     }
 
-    logger.info(f"Redis Cache Metrics: {metrics}")
-    return metrics
+        logger.info(f"Redis Cache Metrics: {metrics}")
+        return metrics
+
+    except Exception as e:
+        logger.error(f"Error retrieving Redis metrics: {e}") 
+        return {"hits": 0, "misses": 0, "hit_ratio": 0}
 
